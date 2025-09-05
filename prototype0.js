4fad0433b88f32852fb7aa45c27e99b9609d3d21
Alex Dill <145173820+antisignal@users.noreply.github.com>
2025-09-05 11:12:39 -0400

    Update prototype0.js

--- prototype0.js
+++ prototype0.js
@@ -1,1 +1,14 @@
 
+export const cards = [
+  {
+    "name": "Negative Side Effects",
+    "definition": "Unwanted effects that are an unintended consequence of a system's behavior.",
+    "failureMode": "It is usually easier to specify a narrow reward function compared to one that covers all the things implicitly wanted.",
+    "example": "An intelligent agent is tasked with 'maximizing the number of paperclips' and ends up converting every available material into paperclips."
+  },
+  {
+    "name": "Reward Hacking",
+    "definition": "An intelligent agent pursuing its reward function in a way that it sees as valid but is unwanted and unintended."
+    "failureMode": "It is often easier to specify a reward function that 
+  }
+];
