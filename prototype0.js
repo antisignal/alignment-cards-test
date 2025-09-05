e7dec0bd8565251c983403a3ea3ff1e250fcac97
Alex Dill <145173820+antisignal@users.noreply.github.com>
2025-09-05 12:18:38 -0400

    Update prototype0.js

--- prototype0.js
+++ prototype0.js
@@ -3,12 +3,19 @@ export const cards = [
   {
     "name": "Negative Side Effects",
     "definition": "Unwanted effects that are an unintended consequence of a system's behavior.",
-    "failureMode": "It is usually easier to specify a narrow reward function compared to one that covers all the things implicitly wanted.",
+    "failureMode": "It is easier to specify a narrow reward function compared to one that covers all the things implicitly wanted.",
     "example": "An intelligent agent is tasked with 'maximizing the number of paperclips' and ends up converting every available material into paperclips."
   },
   {
     "name": "Reward Hacking",
-    "definition": "An intelligent agent pursuing its reward function in a way that it sees as valid but is unwanted and unintended."
-    "failureMode": "It is often easier to specify a reward function that 
+    "definition": "An intelligent agent pursuing its reward function in a way that it sees as valid but is unwanted and unintended.",
+    "failureMode": "A loophole exists in the way the task is specified that allows an agent to complete the task as specified (maximize the reward function), but not complete the intended task.",
+    "example": "The system discovers a software vulnerability that it exploits in order to artificially maximize its reward function.",
+  },
+  {
+    "name": "(Un)safe Exploration",
+    "definition": "Exploratory actions that result in harmful results.",
+    "failureMode": "Agents are not rewarded for avoiding unsafe exploration.",
+    "example": "An agent tasked with overseeing a SQL database attempts a 'drop table' and causes data loss."
   }
 ];
