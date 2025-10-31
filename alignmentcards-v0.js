d0c8599b9eac4d20340211973dfc06df10ace904
Alex Dill <145173820+antisignal@users.noreply.github.com>
2025-10-31 15:44:11 -0400

    Update alignmentcards-v0.js

--- alignmentcards-v0.js
+++ alignmentcards-v0.js
@@ -1,3 +1,5 @@
+// https://docs.google.com/document/d/1UmZ9CL0JNY5s5vGbblkJPr1GggIDfVOnssLSwbzq0vE/edit?tab=t.5ijkmn4gfu0a#heading=h.22cckj8e0t26
+
 export const categories = [
   {
     "code": "AT",  //two characters upper case - these also appear in corresponding cards
@@ -10,8 +12,8 @@ export const categories = [
 
 export const cards = [{
   "category": "AT",
-  "name": "honesty",
-  "definition": "loremipsum",
+  "name": "Honesty",
+  "definition": "Acting with an absence of deceitfulness.",
   "people": "loremipsum",
   "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
   "organizational": "loremipsum",
@@ -32,4 +34,316 @@ export const cards = [{
     "experts": "loremipsum",
     "machine": "loremipsum"
   }
-}]
+}, {
+  "category": "AT",
+  "name": "Conflict Resolution",
+  "definition": "Being willing and able to resolve conflicts peacefully.",
+  "people": "loremipsum",
+  "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+  "organizational": "loremipsum",
+  "organizationalref": "OPTIONAL",
+  "professionals": "loremipsum",
+  "professionalref": "OPTIONAL",
+  "machine": "loremipsum",
+  "machineref": "OPTIONAL",
+  "failureModes": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  },
+    "expand": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  }
+}, {
+  "category": "AT",
+  "name": "Transparent Reasoning",
+  "definition": "Having one's reasoning be visible to others.",
+  "people": "loremipsum",
+  "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+  "organizational": "loremipsum",
+  "organizationalref": "OPTIONAL",
+  "professionals": "loremipsum",
+  "professionalref": "OPTIONAL",
+  "machine": "loremipsum",
+  "machineref": "OPTIONAL",
+  "failureModes": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  },
+    "expand": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  }
+}, {
+  "category": "AT",
+  "name": "Considerateness",
+  "definition": "Thoughtfulness and kindness towards others.",
+  "people": "loremipsum",
+  "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+  "organizational": "loremipsum",
+  "organizationalref": "OPTIONAL",
+  "professionals": "loremipsum",
+  "professionalref": "OPTIONAL",
+  "machine": "loremipsum",
+  "machineref": "OPTIONAL",
+  "failureModes": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  },
+    "expand": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  }
+}, {
+  "category": "AT",
+  "name": "Reliability/Consistency",
+  "definition": "Fulfilling responsibilities and doing as one says they will.",
+  "people": "loremipsum",
+  "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+  "organizational": "loremipsum",
+  "organizationalref": "OPTIONAL",
+  "professionals": "loremipsum",
+  "professionalref": "OPTIONAL",
+  "machine": "loremipsum",
+  "machineref": "OPTIONAL",
+  "failureModes": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  },
+    "expand": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  }
+}, {
+  "category": "AT",
+  "name": "Accountability",
+  "definition": "The acceptance of consequences as a result of failure to perform as needed",
+  "people": "loremipsum",
+  "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+  "organizational": "loremipsum",
+  "organizationalref": "OPTIONAL",
+  "professionals": "loremipsum",
+  "professionalref": "OPTIONAL",
+  "machine": "loremipsum",
+  "machineref": "OPTIONAL",
+  "failureModes": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  },
+    "expand": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  }
+}, {
+  "category": "AT",
+  "name": "Altruism",
+  "definition": "Selfless concern for others.",
+  "people": "loremipsum",
+  "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+  "organizational": "loremipsum",
+  "organizationalref": "OPTIONAL",
+  "professionals": "loremipsum",
+  "professionalref": "OPTIONAL",
+  "machine": "loremipsum",
+  "machineref": "OPTIONAL",
+  "failureModes": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  },
+    "expand": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  }
+}, {
+  "category": "AT",
+  "name": "Conscientiousness",
+  "definition": "Taking obligations seriously, working hard and being careful.",
+  "people": "loremipsum",
+  "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+  "organizational": "loremipsum",
+  "organizationalref": "OPTIONAL",
+  "professionals": "loremipsum",
+  "professionalref": "OPTIONAL",
+  "machine": "loremipsum",
+  "machineref": "OPTIONAL",
+  "failureModes": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  },
+    "expand": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  }
+}, {
+  "category": "AT",
+  "name": "Observantness",
+  "definition": "Paying attention to one's environment and to events and details.",
+  "people": "loremipsum",
+  "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+  "organizational": "loremipsum",
+  "organizationalref": "OPTIONAL",
+  "professionals": "loremipsum",
+  "professionalref": "OPTIONAL",
+  "machine": "loremipsum",
+  "machineref": "OPTIONAL",
+  "failureModes": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  },
+    "expand": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  }
+}, {
+  "category": "AT",
+  "name": "Empathy",
+  "definition": "The ability to put onesself in another's shoes and feel their feelings.",
+  "people": "loremipsum",
+  "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+  "organizational": "loremipsum",
+  "organizationalref": "OPTIONAL",
+  "professionals": "loremipsum",
+  "professionalref": "OPTIONAL",
+  "machine": "loremipsum",
+  "machineref": "OPTIONAL",
+  "failureModes": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  },
+    "expand": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  }
+}, {
+  "category": "AT",
+  "name": "Reciprocity",
+  "definition": "Responding with similar actions (cooperative with cooperative, selfish with selfish)",
+  "people": "loremipsum",
+  "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+  "organizational": "loremipsum",
+  "organizationalref": "OPTIONAL",
+  "professionals": "loremipsum",
+  "professionalref": "OPTIONAL",
+  "machine": "loremipsum",
+  "machineref": "OPTIONAL",
+  "failureModes": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  },
+    "expand": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  }, {
+  "category": "AT",
+  "name": "Shared Intentionality",
+  "definition": "Having the same intentions as something/someone one is aligned with.",
+  "people": "loremipsum",
+  "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+  "organizational": "loremipsum",
+  "organizationalref": "OPTIONAL",
+  "professionals": "loremipsum",
+  "professionalref": "OPTIONAL",
+  "machine": "loremipsum",
+  "machineref": "OPTIONAL",
+  "failureModes": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  },
+    "expand": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  },
+  // omitting trustworthiness because it's too similar to honesty
+  {
+  "category": "AT",
+  "name": "Epistemic humility",
+  "definition": "Knowing when one's knowledge is limited.",
+  "people": "loremipsum",
+  "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+  "organizational": "loremipsum",
+  "organizationalref": "OPTIONAL",
+  "professionals": "loremipsum",
+  "professionalref": "OPTIONAL",
+  "machine": "loremipsum",
+  "machineref": "OPTIONAL",
+  "failureModes": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  },
+    "expand": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  }, {
+  "category": "AT",
+  "name": "Role Fidelity",
+  "definition": "Being faithful and loyal.",
+  "people": "loremipsum",
+  "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+  "organizational": "loremipsum",
+  "organizationalref": "OPTIONAL",
+  "professionals": "loremipsum",
+  "professionalref": "OPTIONAL",
+  "machine": "loremipsum",
+  "machineref": "OPTIONAL",
+  "failureModes": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  },
+    "expand": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  }
+  
+  ]
