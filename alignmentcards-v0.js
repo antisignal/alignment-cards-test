61304a6876f94a2a2b418c2d3e0724774de9f5cd
Alex Dill <145173820+antisignal@users.noreply.github.com>
2025-10-31 15:08:24 -0400

    Modify whitespace

--- alignmentcards-v0.js
+++ alignmentcards-v0.js
@@ -1,1 +1,25 @@
-
+{
+  "category": "AT",
+  "name": "honesty",
+  "definition": "loremipsum",
+  "people": "loremipsum",
+  "peopleref": "OPTIONAL URL reference https://scholarworks.umb.edu/cgi/viewcontent.cgi?referer=&httpsredir=1&article=1001&context=philosophy_faculty_pubs",
+  "organizational": "loremipsum",
+  "organizationalref": "OPTIONAL",
+  "professionals": "loremipsum",
+  "professionalref": "OPTIONAL",
+  "machine": "loremipsum",
+  "machineref": "OPTIONAL",
+  "failureModes": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  },
+    "expand": {
+    "human": "loremipsum",
+    "organizational": "loremipsum",
+    "experts": "loremipsum",
+    "machine": "loremipsum"
+  }
+}
